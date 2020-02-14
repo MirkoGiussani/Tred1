f1c9e31361f86ad9f5d539c7c6cca7bccb2e39d0
giussani.mirko <giussani.mirko@BIBLIO-PC02.OSDBSESTO.local>
2020-02-14 12:37:15 +0100

    add R class implements runnable

--- Tred1/src/tred1/Tred1.java
+++ Tred1/src/tred1/Tred1.java
@@ -13,6 +13,14 @@ public class Tred1 {
         t.start();
         T t2 = new T('b');
         t2.start();
+        
+        R r = new R ('c');
+        Thread t3 = new Thread(r);
+        t3.start();
+        
+        R r2 = new R ('d');
+        Thread t4 = new Thread(r2);
+        t4.start();
     }
     
 }
